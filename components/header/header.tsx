35083c3ed213fc4d436a3b278a75ed9d8dca8d86
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2023-11-10 19:32:44 +0000

    Create header.tsx

---
+++ components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+class Hello extends React.Component {
+  render () {
+    return <div className='message-box'>
+      Hello {this.props.name}
+    </div>
+  }
+}
+const el = document.body
+ReactDOM.render(<Hello name='John' />, el)
