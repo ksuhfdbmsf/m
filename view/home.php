a9ca423e8fc1c5c0e1e884819e987f05e48e9cf8
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2023-11-10 17:15:48 +0000

    Update home.php

--- view/home.php
+++ view/home.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <link rel="stylesheet" href="../components/header/header.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
-</head>
-<main>
+<body>
+
 <?php
-    echo "hello";
-?>
-  <hr>
-  <p>test 6</p>
-</main>
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
 </html>
