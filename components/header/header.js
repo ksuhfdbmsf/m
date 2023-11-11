a04035a05d7c808aac4a94f3338ffefb9b4ee10a
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2023-11-11 14:29:41 +0000

    Update header.js

--- components/header/header.js
+++ components/header/header.js
@@ -6,7 +6,13 @@ function onLoad() {
         <img src="../components/imgs/cat1.png" alt="..." width="50px" height="50px">
       </div>
       <div class="links">
-        <p><span class="linkSpan">ABCDE</span><span class="linkSpan">FGHIJ</span><span class="linkSpan">KLMNO</span></p>
+        <p>ABCDE</p>
+      </div>
+      <div class="links">
+        <p>FGHIJ</p>
+      </div>
+      <div class="links">
+        <p>KLMNO</p>
       </div>
       <div class="search">
         <p>Search</p>
