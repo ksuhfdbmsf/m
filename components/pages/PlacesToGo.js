770f023e0070a599f279cbf50f1ae872e479cbce
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2024-05-25 20:41:06 +0100

    Update PlacesToGo.js

--- components/pages/PlacesToGo.js
+++ components/pages/PlacesToGo.js
@@ -2,7 +2,7 @@ let oldElm = "AuBtn";
 function prceBtn(id, colour) {
     document.getElementById(id).style.background = colour;
     document.getElementById(oldElm).style.background = '#FFFFFF';
-  if (document.getElementById(oldElm).style.background == white) {
+  if (document.getElementById(oldElm).style.background == '#FFFFFF') {
     oldElm = id;
     }
 }
