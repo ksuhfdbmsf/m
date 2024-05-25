d2d36b21d8263559af703680e734273a90dcb5c2
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2024-05-25 17:49:36 +0100

    Update PlacesToGo.js

--- components/pages/PlacesToGo.js
+++ components/pages/PlacesToGo.js
@@ -1,6 +1,7 @@
 var oldElm;
 function prceBtn(element, colour) {
-    oldElm.style.background = transparent;
+//    oldElm.style.background = transparent;
     element.style.background = colour;
+    element.innerHTML = oldElm;
     oldElm = element;
 }
