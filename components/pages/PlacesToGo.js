a7f92136264b72b8531760f9bc7dcaddf375ec52
ywergsifukhdj <50847213+ksuhfdbmsf@users.noreply.github.com>
2024-05-26 14:47:30 +0100

    Update PlacesToGo.js

--- components/pages/PlacesToGo.js
+++ components/pages/PlacesToGo.js
@@ -4,9 +4,9 @@ function prceBtn(id, colour, info) {
     document.getElementById(id).style.background = colour;
   if (oldElm != null) {
     document.getElementById(oldElm).style.background = '';
-  //  document.getElementById(olnfo).style.display = '';
+    document.getElementById(oldInfo).style.display = '';
   }
- //   document.getElementById(info).style.display = 'block';
+    document.getElementById(info).style.display = 'block';
     
     oldElm = id;
     oldInfo = info;
